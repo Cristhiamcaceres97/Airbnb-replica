5f77245911e3b210a121285f7ec56090debea04d
Cristhiamcaceres97 <133033840+Cristhiamcaceres97@users.noreply.github.com>
2023-07-18 15:56:00 -0600

    commit martes

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -1,35 +1,61 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-const CardComponent = ({ img, ubicacion, distancia, dias, precio }) => {
+const CardComponent = ({ images, ubicacion, distancia, dias, precio }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
     <div className="card border-0" style={{ width: "18rem", marginTop: "2rem" }}>
-      <div style={{ overflow: "hidden", width: "100%", height: "300px" }}>
-        <img
-          src={img}
-          className="card-img-top"
-          alt="Ubicación"
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            borderRadius: "10px",
-          }}
-        />
+      <div style={{ overflow: "hidden", width: "170%", height: "285px" }}>
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image}
+                className="card-img-top"
+                alt={`Image ${index}`}
+                style={{ width: "295px", height: "285px", objectFit: "cover", borderRadius: "10px" }}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
       <div className="card-body">
-        <h5 className="card-title" style={{ fontSize: "1rem" }}>
-          {ubicacion}
-        </h5>
-        <p className="card-text" style={{ fontSize: "0.8rem" }}>
-          Distancia: {distancia}
-        </p>
-        <p className="card-text" style={{ fontSize: "0.8rem" }}>
-          Fechas: {dias}
-        </p>
-        <p className="card-text" style={{ fontSize: "0.8rem" }}>
-          Precio: <span style={{ fontWeight: "bold" }}>{precio}</span> noche
-        </p>
+        <h5 className="card-title" style={{ fontSize: "1rem" }}>{ubicacion}</h5>
+        <p className="card-text" style={{ fontSize: "0.8rem" }}>Distancia: {distancia}</p>
+        <p className="card-text" style={{ fontSize: "0.8rem" }}>Fechas: {dias}</p>
+        <p className="card-text" style={{ fontSize: "0.8rem" }}>Precio: <span style={{ fontWeight: "bold" }}>{precio}</span> noche</p>
       </div>
+    </div>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <FontAwesomeIcon icon={faChevronLeft} />
+    </div>
+  );
+};
+
+const NextArrow = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <FontAwesomeIcon icon={faChevronRight} />
     </div>
   );
 };
@@ -39,7 +65,7 @@ const DataCards = ({ data }) => {
     <div className="row g-1">
       {data.map((item, index) => (
         <div className="col-3" key={index}>
-          <CardComponent {...item} />
+          <CardComponent images={item.images} {...item} />
         </div>
       ))}
     </div>
